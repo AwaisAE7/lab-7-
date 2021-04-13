beb46b2c2b2b231ab62e5c0c0e6560197a410cc5
AwaisAE7 <82449704+AwaisAE7@users.noreply.github.com>
2021-04-13 22:27:35 +0500

    lab 7 task 3 done
    task 3 wala commit

--- App.js
+++ App.js
@@ -1,8 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { Alert, Modal, Pressable, StyleSheet, Text, View } from 'react-native';
 
+
+
+var index = ["","","","","","","","",""];
+
 export default function App() {
+  const [getText, setText] = useState("x")
+  const [gettry, settry]= useState(1)
+  const [getturn, setturn]= useState(1)
+  
+ 
+  const buttonClick = (txt) => {
+if(index[txt].length<1){ if(gettry % 2 == 0){
+  setturn(2);
+  setText("x");
+  
+  index[txt]=getText;
+  
+  
+     }
+     else{
+      setturn(1);
+  setText("0");
+  
+  index[txt]=getText;
+  
+     }
+     settry(gettry+1);
+    }else{
+      alert("already pressed sheeda jii!!");
+    }
+  
+  }
+  
+  const check = ()=>{
+
+
+  }
   return (
     <View style={styles.container}>
 <View style={styles.play}>
@@ -10,47 +46,49 @@ export default function App() {
           <Text  style={{marginRight: 25}}>ALI BHAI: X</Text>
           <Text>OMER BHAI: O</Text>
         </Text>
-     
+    
         
       </View>
+      <View><Text>TURN:PLAYER {getturn}</Text></View>
+      
       <View >
         <Text  style={{ fontSize: 20, fontWeight: 'bold', margin:20}}>TIC TAC TOE WALI GAME</Text>
         <Text >----------------------------------------------------</Text>
       </View>
       <View>
         <View style={styles.R1}>
-          <Pressable style={styles.buttons} onPress={""}>
-            X
+          <Pressable style={styles.buttons} onPress={()=>{buttonClick(0)}}>
+            {index[0]}
           </Pressable>
-          <Pressable style={styles.buttons} onPress={""}>
-          0
+          <Pressable style={styles.buttons} onPress={()=>{buttonClick(1)}}>
+          {index[1]}
           </Pressable>
-          <Pressable style={styles.buttons} onPress={""}>
-          X
-          </Pressable>
-        </View>
-
-        <View style={styles.R1}>
-          <Pressable style={styles.buttons} onPress={""}>
-          0
-          </Pressable>
-          <Pressable style={styles.buttons} onPress={""}>
-          0
-          </Pressable>
-          <Pressable style={styles.buttons} onPress={""}>
-          X
+          <Pressable style={styles.buttons} onPress={()=>{buttonClick(2)}}>
+          {index[2]}
           </Pressable>
         </View>
 
         <View style={styles.R1}>
-          <Pressable style={styles.buttons} onPress={""}>
-          0
+          <Pressable style={styles.buttons} onPress={()=>{buttonClick(3)}}>
+          {index[3]}
           </Pressable>
-          <Pressable style={styles.buttons} onPress={""}>
-          X
+          <Pressable style={styles.buttons} onPress={()=>{buttonClick(4)}}>
+          {index[4]}
           </Pressable>
-          <Pressable style={styles.buttons} onPress={""}>
-          0
+          <Pressable style={styles.buttons} onPress={()=>{buttonClick(5)}}>
+          {index[5]}
+          </Pressable>
+        </View>
+
+        <View style={styles.R1}>
+          <Pressable style={styles.buttons} onPress={()=>{buttonClick(6)}}>
+          {index[6]}
+          </Pressable>
+          <Pressable style={styles.buttons} onPress={()=>{buttonClick(7)}}>
+          {index[7]}
+          </Pressable>
+          <Pressable style={styles.buttons} onPress={()=>{buttonClick(8)}}>
+          {index[8]}
           </Pressable>
         </View>
       </View>
