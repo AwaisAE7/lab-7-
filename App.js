c54bf71f027118f96a2070cbfe8070dc3ba37450
AwaisAE7 <82449704+AwaisAE7@users.noreply.github.com>
2021-04-15 17:03:25 +0500

    lab 7 TASK 5
    LAB 7 complete

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ export default function App() {
   const [gettry, settry]= useState(1)
   const [getturn, setturn]= useState(1)
   const [getwinner, setwinner]= useState("")
+  const [modalVisible, setModalVisible] = useState(false);
 
   const buttonClick = (txt) => {
     
@@ -40,48 +41,72 @@ if(index[txt].length<1){ if(gettry % 2 == 0){
   }
   
   const check = ()=>{
-    
-if((index[0]=="X" && index[1]=="X" && index[2]=="X") || (index[0]=="X" && index[3]=="X" && index[6]=="X") || (index[0]=="X" && index[4]=="X" && index[8]=="X" ) || (index[1]=="X" && index[4]=="X" && index[7]=="X" ) || (index[2]=="X" && index[5]=="X" && index[8]=="X" ) || (index[3]=="X" && index[4]=="X" && index[5]=="X" ) ||(index[6]=="X" && index[7]=="X" && index[8]=="X" ) || (index[2]=="X" && index[4]=="X" && index[6]=="X" )){
+if((index[0]=="X" && index[1]=="X" && index[2]=="X") || (index[0]=="X" && index[3]=="X" && index[6]=="X") || (index[0]=="X" && index[4]=="X" && index[8]=="X" ) || (index[1]=="X" && index[4]=="X" && index[7]=="X" ) || (index[2]=="X" && index[5]=="X" && index[8]=="X" ) || (index[3]=="X" && index[4]=="X" && index[5]=="X" ) || (index[6]=="X" && index[7]=="X" && index[8]=="X" ) || (index[2]=="X" && index[4]=="X" && index[6]=="X" )){
 setwinner("ALI BHAI won congrats!!!");
+setModalVisible(true);
 }
-else if((index[0]=="0" && index[1]=="0" && index[2]=="0") || (index[0]=="0" && index[3]=="0" && index[6]=="0") || (index[0]=="0" && index[4]=="0" && index[8]=="0" ) || (index[1]=="0" && index[4]=="0" && index[7]=="0" ) || (index[2]=="0" && index[5]=="0" && index[8]=="0" ) || (index[3]=="0" && index[4]=="X" && index[5]=="0" ) ||(index[6]=="0" && index[7]=="0" && index[8]=="0" ) || (index[2]=="0" && index[4]=="0" && index[6]=="0" )){
+else if((index[0]=="0" && index[1]=="0" && index[2]=="0") || (index[0]=="0" && index[3]=="0" && index[6]=="0") || (index[0]=="0" && index[4]=="0" && index[8]=="0" ) || (index[1]=="0" && index[4]=="0" && index[7]=="0" ) || (index[2]=="0" && index[5]=="0" && index[8]=="0" ) || (index[3]=="0" && index[4]=="0" && index[5]=="0" ) || (index[6]=="0" && index[7]=="0" && index[8]=="0" ) || (index[2]=="0" && index[4]=="0" && index[6]=="0" )){
   setwinner("OMER BHAI won congrats!!!");
+  setModalVisible(true);
 }
-
 else{
-  for (let i = 0; i < index.length; i++) {
-    if (index[i].length < 1) {
-      setwinner("");
-    }else{
-      setwinner("DRAW");
+  var count=0;
+  for (let i = 0; i < 9; i++) {
+    if ((index[i] == "X" || index[i] == "0") ){
+    count++;   
     }
- 
-  
+  }if(count==9){
+    setwinner("DRAW");
+    setModalVisible(true);
+  }
+  else{setwinner("");
+}  
 }
 
   
-  
-}
+
+
    
   }
 
 
   return (
     <View style={styles.container}>
+       <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+          <Text  style={{ fontSize: 20, fontWeight: 'bold', margin:20,color: 'red', justifyContent: 'center', alignItems: 'center' }}>{getwinner}</Text>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => {setModalVisible(!modalVisible),index=["","","","","","","","",""],setwinner("")}}
+            >
+              <Text style={styles.textStyle}>PLAY AGAIN!!!</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
 <View style={styles.play}>
-        <Text style={{ fontSize: 20, marginRight: 25}}>
+        <Text style={{ fontSize: 20, marginRight: 25,color:'white'}}>
           <Text  style={{marginRight: 25}}>ALI BHAI: X</Text>
           <Text>OMER BHAI: O</Text>
         </Text>
     
         
       </View>
-      <View><Text style={{ fontSize: 20}}>TURN:<Text style={{ fontSize: 20, fontWeight: 'bold', margin:20}}>PLAYER {getturn}</Text></Text></View>
+      <View><Text style={{ fontSize: 20,color:'white'}}>TURN:<Text style={{ fontSize: 20, fontWeight: 'bold', margin:20}}>PLAYER {getturn}</Text></Text></View>
       
       <View >
-        <Text  style={{ fontSize: 20, fontWeight: 'bold', margin:20}}>TIC TAC TOE WALI GAME</Text>
-        <Text >----------------------------------------------------</Text>
-        <Text  style={{ fontSize: 20, fontWeight: 'bold', margin:20,color: 'red', justifyContent: 'center', alignItems: 'center' }}>{getwinner}</Text>
+        <Text  style={{ fontSize: 20, fontWeight: 'bold', margin:20,color:'white'}}>TIC TAC TOE WALI GAME</Text>
+        <Text style={{color:'white'}}>----------------------------------------------------</Text>
+        {/* <Text  style={{ fontSize: 20, fontWeight: 'bold', margin:20,color: 'red', justifyContent: 'center', alignItems: 'center' }}>{getwinner}</Text> */}
       </View>
       <View>
         <View style={styles.R1}>
@@ -131,9 +156,30 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
    
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "black",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "red",
+    shadowOffset: {
+      width: 20,
+      height: 20
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
   },
   buttons: {
     fontSize: 35,
@@ -152,7 +198,27 @@ const styles = StyleSheet.create({
   },
   R1: {
     flexDirection: 'row',
-  }
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
+  },
+  button: {
+    borderRadius: 100,
+    padding: 5,
+    elevation: 5
+  },
+  buttonOpen: {
+    backgroundColor: "green",
+  },
+  buttonClose: {
+    backgroundColor: "green",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
  
   
 });
